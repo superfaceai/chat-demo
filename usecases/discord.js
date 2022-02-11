4b679a0ffee100a9f17adbcebf5bed21a4214b47
Martin Albert <martin.albert.187@gmail.com>
2022-02-11 12:32:06 +0100

    feat: update discord example

--- usecases/discord.js
+++ usecases/discord.js
@@ -1,6 +1,13 @@
 const { inspect } = require('util');
 
-const { getMessages, getDestinations, sendMessage, getServers } = require('./sf/usecases');
+const {
+  getChannels,
+  getMembers,
+  getMessages,
+  getServers,
+  getThreads,
+  sendMessage,
+} = require('./sf/usecases');
 
 async function discord(destination) {
   const provider = 'discord';
@@ -8,10 +15,11 @@ async function discord(destination) {
   const servers = await getServers(provider, { asBot: true });
   // console.log(inspect(servers, true, 15));
 
-  const destinations = await getDestinations(provider, { server: servers.servers[0].id });
-  // console.log(inspect(destinations, true, 15));
+  const serverId = servers.servers[0].id;
+  const channels = await getChannels(provider, { server: serverId });
+  // console.log(inspect(channels, true, 15));
 
-  const channelId = destinations.destinations[0].id;
+  const channelId = channels.channels[0].id;
   const firstPage = await getMessages(provider, {
     destination: channelId,
     limit: 2,
@@ -19,35 +27,49 @@ async function discord(destination) {
 
   console.log(inspect(firstPage, true, 15));
 
-  // To use pagination with discord, 
-  // you need to setup afterDate or beforeDate
-  // based on direction you choose.
+  // To use pagination with discord,
+  // you need to setup parameter page. (similar to slack)
 
   // For pagination forwards, take nextPage from previous result
-  // and set it up as beforeDate
-  const nextPage = await getMessages(provider, {
-    destination: channelId,
-    beforeDate: firstPage.nextPage,
-    limit: 2,
-  })
+  // and set it up as page.
+  // For pagination backwards, you have to have cursor stored
+  // from previous calls.
+  if (firstPage.nextPage !== undefined) {
+    const nextPage = await getMessages(provider, {
+      destination: channelId,
+      page: firstPage.nextPage,
+      limit: 2,
+    });
 
-  console.log(inspect(nextPage, true, 15))
+    console.log(inspect(nextPage, true, 15));
+  }
 
-  // For pagination backwards, take previousPage
-  // and set it as afterDate
-  const previousPage = await getMessages(provider, {
-    destination: channelId,
-    afterDate: nextPage.previousPage,
-    limit: 2,
-  })
-
-  console.log(inspect(previousPage, true, 15))
-
+  // SendMessage
   // const sentMessage = await sendMessage(provider, {
   //   destination,
   //   text: 'text message',
   // });
   // console.log(inspect(sentMessage, true, 15));
+
+  // new usecases:
+  // GetMembers - also supports pagination, in same manner as described above
+
+  const members = await getMembers(provider, {
+    server: serverId,
+    limit: 2,
+  });
+
+  console.log(inspect(members, true, 15));
+
+  // GetThreads - this pulls currently active threads
+  // this usecase does not support pagination as well as in
+  // discord map for getting channels, they return them all in one request
+  const threads = await getThreads(provider, {
+    server: serverId,
+    limit: 2,
+  });
+
+  console.log(inspect(threads, true, 15));
 }
 
 module.exports = discord;
